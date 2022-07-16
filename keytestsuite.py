ec3d8f12fec48b2127b11438d11c66b21bee6313
AAK05 <adrien.kusuma@gmail.com>
2022-07-16 11:01:31 +0700

    Added difftests.py to test difference between A[i+1] and A[i], as well as slope trajectory between A[i] and A[i+1] to attempt prediction A[i+2]
    Modified keytestsuite.py to call aforementioned difftests.py

--- keytestsuite.py
+++ keytestsuite.py
@@ -2,6 +2,7 @@ from autocorrelation import *
 from dig_transition import *
 from freq_analysis import *
 from montecarlopitest import *
+from difftests import *
 import sys
 import json
 
@@ -64,11 +65,15 @@ for i in range(3*(len(data2)//3)):
     if i%3 == 0:
         n = (data2[i]) + (data2[i+1]) + (data2[i+2]) #+ (data2[i+3]) + (data2[i+4])
         datax2.append(int(n))"""
-        
+
 print("Maximum autocorrelation (k,autocorrelation): " + str(max_autocorr(data1,20)))
 print("Minimum autocorrelation (k,autocorrelation): " + str(min_autocorr(data1,20)))
 print("Maximum pattern error from expected (pattern,%): " + str(max_pattern(data1)))
 print("Minimum pattern error from expected (pattern,%): " + str(min_pattern(data1)))
+print("Max diff error (diff,%): " + str(max_diff_error(data1)))
+print("Min diff error (diff,%): " + str(min_diff_error(data1)))
+print("Max slope error(slope,%): " + str(max_slope_error(data1)))
+print("Min slope error(slope,%): " + str(min_slope_error(data1)))
 print("Maximum frequency error (digit,%): " + str(max_freq_err(data1)))
 print("Minimum frequency error (digit,%): " + str(min_freq_err(data1)))
 print("Simulated value of pi: " + str(MonteCarloPi(datax1,datax2)))
