d5d0e9e92ab7f4febde80610923647844d25b03b
AAK05 <adrien.kusuma@gmail.com>
2022-07-16 16:49:16 +0700

    Modified difftests.py to detect data base10 and data base26, adjust modulo accordingly

--- difftests.py
+++ difftests.py
@@ -1,14 +1,26 @@
 def difftest(data):
     dist = {}
+    b26 = False
+    if max(data) > 10:
+        b26 = True
     for i in range(len(data)-1):
-        diff = (data[i+1] - data[i]) % 26
+        if b26 == True:
+            diff = (data[i+1] - data[i]) % 26
+        else:
+            diff = (data[i+1] - data[i]) % 10
         dist[diff] = dist.get(diff,0) + 1
     return dist
 
 def slopetest(data):
     dist = {}
+    b26 = False
+    if max(data) > 10:
+        b26 = True
     for i in range(len(data)-2):
-        slopediff = (data[i+2] - 2*data[i+1] + data[i]) % 26
+        if b26 == True:
+            slopediff = (data[i+2] - 2*data[i+1] + data[i]) % 26
+        else:
+            slopediff = (data[i+2] - 2*data[i+1] + data[i]) % 10
         dist[slopediff] = dist.get(slopediff,0) + 1
     return dist
 
