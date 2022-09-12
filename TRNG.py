99f169965b2f77c3bc1b711b3df9928381dba90c
AAK05 <adrien.kusuma@gmail.com>
2022-09-12 22:48:38 +0700

    Edited encryption2.py to use new encryption key with 3[sqrt(2)] elements Caused edits in Amber-Spyglass-Encrypted.txt and Amber-Spyglass_Encrypted-Enumerated.json Created TRNG.py and TRNG2.py TRNG.py created randorgTRNG.json, which was used to generate randorgTRNG2.json and randorgTRNG3.json TRNG2.py created TRNG2.json TRNG.py uses diy b26 conversion. TRNG2.py uses Nerdull TRNG converter Also created sysTRNG.json, which uses random.Systemrandom() to generate

--- TRNG.py
+++ TRNG.py
@@ -14,5 +14,19 @@ def sysrandom(len=1000000):
         sys.append(rng.randint(1,26))
     return sys
 
+def randorg():
+    output = []
+    with open("2022-08-01.txt","r") as file:
+        bitsource = file.readline()
+    decimal = int(bitsource,2)
+    print("Converting randorgTRNG to base26")
+    #Convert to base26
+    while decimal != 0:
+        output.append(decimal%26)
+        decimal = decimal//26
+    output = output[::-1]
+    return output
+
 if __name__ == "__main__":
     writejson(sysrandom(),"sysTRNG.json")
+    writejson(randorg(),"randorgTRNG.json")
